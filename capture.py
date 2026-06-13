aba9b6714ed81e3ea999df62334af1f906c9b8f4
Alessandro Tarantino <tarantino.alessandro14@gmail.com>
2026-06-13 15:24:55 +0200

    capture.py modified for the tcp analysis

--- capture.py
+++ capture.py
@@ -2,6 +2,7 @@ import pyshark
 import asyncio #manage the asynchronicity
 #main and useful library for capturing net packets
 #project will be developed in classes
+from analyzers.tcp_tracker import TCPTracker
 
 class Capture:
     def __init__(self, pcap_path): #define a class in which function will be passed the pcap file path
@@ -17,25 +18,22 @@ class Capture:
         
         print(f"Analyzing {self.pcap_path}...\n")
 
+        tracker = TCPTracker()
         try:
-            capture = pyshark.FileCapture(self.pcap_path) #FileCapture function in pyshark, reads the pcap
+            capture = pyshark.FileCapture(self.pcap_path, display_filter="tcp") #FileCapture function in pyshark, reads the pcap
 
             pack_c = 0
             for packet in capture:
                 if pack_c >= pack_l:
                     break
 
-                if hasattr(packet, "ip"): #verifies that packet has IP level before extracting data - hasattr (has attribute)
-                    src_ip = packet.ip.src #assignments
-                    dst_ip = packet.ip.dst
-                    protocol = packet.highest_layer
-
-                    print(f"[{pack_c + 1}] {src_ip} --> {dst_ip} | Protocol: {protocol}")
+                tracker.track_pack(packet)
 
                 pack_c += 1
 
             capture.close()
             print("\nBasic analysis completed.")
+            print(f"Unique TCP sessions detected: {len(tracker.sessions)}")
         
         except FileNotFoundError:
             print(f"Err: file {self.pcap_path} does not exists.")
